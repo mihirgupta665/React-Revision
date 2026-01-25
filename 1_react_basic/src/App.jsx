958a27927231f3f9ab9a9253359200ab84f2ce16
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-25 19:04:12 +0530

    Creating 1st component

--- 1_react_basic/src/App.jsx
+++ 1_react_basic/src/App.jsx
@@ -1,8 +1,24 @@
 import "./App.css"
 
+function Title() {
+    return <h1>Hello World! this side Mihir</h1>
+}
+
+function Description() {
+    return <p>This is the description rendering</p>
+}
+
 function App() {  // App function is itself the app component   
-    return <h1>Hello World! this side Mihir</h1>;   // whatever is returned becomes the visible UI of the page
+    return (        // only one root element could be returned at a time
+        <div>
+            <Title></Title>
+            <Description />
+            <Description />
+            <h3>So lets Go!!</h3>
+        </div>
+    );   // whatever is returned becomes the visible UI of the page
 
 }
 
 export default App
+// there are two ways of rendering <title></title> or <title/>
