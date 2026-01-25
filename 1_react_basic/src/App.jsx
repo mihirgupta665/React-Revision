d0e6a62a9fd88dc8342ea2a33c565eccf49b84b4
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-25 20:45:20 +0530

    unsing Curly braces in JSX to create space for pure JS

--- 1_react_basic/src/App.jsx
+++ 1_react_basic/src/App.jsx
@@ -2,9 +2,10 @@
 Writing Markup Rules of JSX: 
 i> one root element could only be returned
 ii> all tags must be closed
-ii> use Camel case most of the time
-
+iii> use Camel case most of the time
 */
+
+// Curly braces in JSX becomes the window for JS, everycode in curly braces it treated as pure JS
 
 import "./App.css"
 import {Author} from "./Author.jsx";
@@ -18,14 +19,17 @@ function Description() {
 }
 
 function App() {  // App function is itself the app component   
+    let name = "Master Mihir"
     return (        // only one root element could be returned at a time
-        <div>
+        // <> </> is the React fragment use to wrap multiple elements with out creating a separate new node
+        <>  
             <Title></Title>
             <Description />
             <Description />
             <Author/>
             <h3>So lets Go!!</h3>
-        </div>
+            { name.toUpperCase() }     
+        </>
     );   // whatever is returned becomes the visible UI of the page
 
 }
