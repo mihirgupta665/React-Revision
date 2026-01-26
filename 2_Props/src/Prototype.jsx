e128bf21e581e624476eb2c9e06e7e5aab202e60
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-26 18:32:56 +0530

    Creating dynamic styled components

--- 2_Props/src/Prototype.jsx
+++ 2_Props/src/Prototype.jsx
@@ -7,9 +7,14 @@ import "./Prototype.css"
 
 //conditionals should be applied as directly in ternary operator inside curply braces to render something in specific conditions, if true element should be created else null shoud be returned maintaining the DOM
 
+// dynamic styling => style = {object} : style attribute is used to assign dynamic styling
+// object shoule contain key as CSS property in Camel case and its value in strings, conditions could also be written in the ternary operator in value of the key
+
 function Prototype({title, price, list1=["Technology"], features={name:"Mihir", age:21}}){
+    let styles = {backgroundColor : price > 70000 ? "aqua":"", color: price>70000 && "black"};
+
     return (
-        <div className="Prototype">
+        <div className="Prototype" style={styles}>
             <h3>The Product is : {title}</h3>
             <p>The Price for the product is : {price}</p>
             <h5>List : {list1}</h5>
