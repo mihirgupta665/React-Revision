c61c81a0aa22f06b3d95dc9583404eb0113e6cf1
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-27 00:18:19 +0530

    Creating a react activity app for products tab

---
+++ 3_ProductTab_Activity/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ProductTab from "./ProductTab.jsx";
+
+export default function App() {
+    return (
+        <>
+        <h2>BlockBuster Deal | Shop Now!</h2>
+        <ProductTab/>
+        </>
+    );
+}
---
+++ 3_ProductTab_Activity/src/Product.jsx
@@ -0,0 +1,19 @@
+import "./Product.css";
+import Price from "./Price.jsx";
+
+export default function Product({ title, idx }) {
+    let oldPrice = ["12,495", "11,900", "1,599", "599"];
+    let newPrice = ["8,999", "9,199", "899", "278"];
+    let description = [["8,000 DPI", "5 Programmable Buttons"],
+    ["intuitive surface", "designed for iPad Pro"],
+    ["designed for iPad Pro", "intuitive surface"],
+    ["wireless", "optical orientation"]];
+    return (
+        <div className="Product">
+            <h4>{title}</h4>
+            <p>{description[idx][0]}</p>
+            <p>{description[idx][1]}</p>
+            <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
+        </div>
+    );
+}
