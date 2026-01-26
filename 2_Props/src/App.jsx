7068752fa9c1fe8fe5f226048505f4b78d2dc219
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-26 18:39:14 +0530

    Creating a custom dynamic components for greeting

--- 2_Props/src/App.jsx
+++ 2_Props/src/App.jsx
@@ -1,8 +1,14 @@
-import "./App.css"
-import PrototypeTab from "./PrototypeTab.jsx"
+import "./App.css";
+import PrototypeTab from "./PrototypeTab.jsx";
+import MsgBox from "./MsgBox.jsx";
+
 function App(){
     return (
+        <>
+        <MsgBox UserName="Mihir" TextColor="Yellow" />
         <PrototypeTab/>
+        <MsgBox UserName="Divyansh" TextColor="Green" />
+        </>
     );
 }
 
--- 2_Props/src/MsgBox.jsx
+++ 2_Props/src/MsgBox.jsx
@@ -1,3 +1,9 @@
-export default function MsgBox(UserName, ){
+// props are passed in curly braces for JS window
+// style attributes take object inside JS window so two curly braces
 
+export default function MsgBox({UserName, TextColor}){
+    
+    return (
+        <h1 className="TextColor" style={{color:TextColor}}>Welcome, {UserName}</h1>
+    );
 }
