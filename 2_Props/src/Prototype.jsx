175f187874efd11bc49900a331949100b3e2a9fc
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-26 12:06:44 +0530

    rendering arra with html elements

--- 2_Props/src/Prototype.jsx
+++ 2_Props/src/Prototype.jsx
@@ -1,12 +1,17 @@
 import "./Prototype.css"
 
+// except string everything is written inside curly braces
+// default values to the props could also be given
+// object is not printed as whole it is printed with keys only
+// in react array could have html element also as their values
+
 function Prototype({title, price, list1=["Technology"], features={name:"Mihir", age:21}}){
     return (
         <div className="Prototype">
             <h3>The Product is : {title}</h3>
             <p>The Price for the product is : {price}</p>
             <h5>List : {list1}</h5>
-            <h6>Featured by : {features.name} of {features.age}</h6>
+            <h6>Featured by : {features.name} of {features.age}</h6>  
         </div>
     );
 }
--- 2_Props/src/PrototypeTab.jsx
+++ 2_Props/src/PrototypeTab.jsx
@@ -1,6 +1,8 @@
 import Prototype from "./Prototype.jsx"
 function PrototypeTab() {
-    let list1 = ["Hi-tech", "Durable", "Expensive"];
+    let list1 = [<li>"Hi-tech"</li>, <li>"Durable"</li>, <li>"Expensive"</li>];
+    // or could use map function to change a normal array to html containing element array
+    // list2 = list1.map( (element)=> <li>element</li> )   // this changes original element to html element
     return (
         <>
             <Prototype title="Laptop" price={150000} list1={list1} features={{name:"Mihir", age:21}} />
