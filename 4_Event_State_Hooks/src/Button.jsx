e9e9185dfddeddbf027a31e5cd70c8d743bab97b
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-27 13:27:55 +0530

    Creating onmouseover and dblclick events

--- 4_Event_State_Hooks/src/Button.jsx
+++ 4_Event_State_Hooks/src/Button.jsx
@@ -1,20 +1,30 @@
 
-function printHello() {
+function handleClick() {
     console.log("Hello! I was clicked!!");
 }
 
-function printBye() {
+function handleMouseOver() {
     console.log("Byeeee!!!");
 }
 
+function handleDblClick(){
+    console.log("Yippey! I was clicked Twice!!");
+}
+
+// generaly different events are attached with "on" for event handling and name to the function is given by handleEvent
+// For eg: to handle "click" event we have attribute "onClick" and name of function might be "handleClick"
+// only reference of the function should be given, function should not be executed (don't write "()" at last) otherwise function will be executed when page loads and will not tigger when needed
 // onClick could be assigned to any html element for tiggering a task whenever a clicked is made
+// onMouseOver is used for hover with mouse
 
 export default function Button(){
     return(
         <div>
-            <button  onClick={printHello}>Click me!</button>
+            <button  onClick={handleClick}>Click me!</button>
             <br />
-            <p onClick={printBye}>Hi! there i am a para!!</p>
+            <p onMouseOver={handleMouseOver}>Hi! there i am a para!!</p> 
+            <br />
+            <button onD oubleClick={handleDblClick}>Double Click Me!</button>
         </div>
     );
 }
