5cb6cc0b73481e378bf1f89b95982677f949df75
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-28 14:20:58 +0530

    Creating states and functionality for todo list and new task

--- 7_TodoList_App/src/ToDoApp.jsx
+++ 7_TodoList_App/src/ToDoApp.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+
 export default function ToDoApp(){
+    let [todos, setTodos] = useState(["Sample Task"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTodo = () => {
+        setTodos(() => {
+            return [...todos, newTodo];
+        });
+        setNewTodo("");
+    }
+
+    let updateTodoValue = (event) => {
+        // console.log(event.target.value);
+        setNewTodo(event.target.value);
+    }
+
     return (
         <>
-            <input type="text" placeholder="Enter the Task" />
+            <input type="text" placeholder="Enter the Task" value={newTodo} onChange={updateTodoValue} />
             <br />
-            <button>Add Task</button>
+            <button onClick={addNewTodo}>Add Task</button>
             <br />
             <br />
             <br />
             <hr />
             <h1>ToDo List</h1>
-            <ul></ul>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li>{todo}</li>
+                    ))
+                }
+            </ul>
         </>
     );
 }
