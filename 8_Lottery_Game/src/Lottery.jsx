ff073c84ea90b28802298f24621c7e8ccae05412
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-29 12:19:54 +0530

    creating winning functionality

--- 8_Lottery_Game/src/Lottery.jsx
+++ 8_Lottery_Game/src/Lottery.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import "./Lottery.css";
 import { genTicket, sum } from "./helper.js";
 
+// Lifting State Up :  when two components needs to be change together then remove state from both and assign the state to their closest parent and pass those state to them using props
+// whenever writing a component : always decide it design by three major things: i> props, ii> state, iii> events
+
 export default function Lottery(){
 
     let [ticket, setTicket] = useState(genTicket(3));
