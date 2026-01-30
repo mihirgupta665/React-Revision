4a5dea4db02d8bb464bc01fcfc4414f3aa88157f
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-30 11:02:11 +0530

    handle common handlers for input change and form submit

--- 9_Forms_Labels/src/Form.jsx
+++ 9_Forms_Labels/src/Form.jsx
@@ -5,11 +5,12 @@ export default function Form() {
     // let [username, setUsername] = useState("");
     let [formData, setFormData] = useState({
         fullName: "",
-        username: ""
+        username: "",
+        password: ""
     })
 
     /*
-    Mechanis of flow: i> input change, ii> onchange tiggers, iii> handleNameChange gets invoked,
+    Mechanism of flow: i> input change, ii> onchange tiggers, iii> handleNameChange gets invoked,
     <iv>setFullName executes, v> fullName(state) values changes, vi> react re-renders due to state change
     */
     // let handleNameChange = (event) => {
@@ -21,27 +22,44 @@ export default function Form() {
     //     setUsername(event.target.value);
     // }
 
+    // for handle multiple input write each element name in object and change the state of object and then could create common handlers
     let handleInputChange = (event) => {
         let eventName = event.target.name;
         let eventNewValue = event.target.value;
         setFormData((currData) => {
-            currData[eventName] = eventNewValue;
-            return {...currData};
+            // change value using [] sq brakets not dot operator and sq braces are used to compute the variable and then vlaue is assgined where as dot directyl assign the value.
+            // currData[eventName] = eventNewValue;
+            return {...currData, [eventName] : eventNewValue};
+        })
+    }
+
+    // preventDefault() : prevents the default behaviour of forms to submit the form and refresh the page
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            username: "",
+            password: ""
         })
     }
 
 
     return(
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 {/* for is an loop reserved keyword in Js therefore "htmlFor" is used to denote so */}
                 <label htmlFor="fullName">Enter Name : </label>
-                <input id="fullName" type="text" placeholder="Enter your name..." value={fullName} onChange={handleInputChange} name="fullName" />
+                <input id="fullName" type="text" placeholder="Enter your name..." value={formData.fullName} onChange={handleInputChange} name="fullName" />
                 <br />
                 <br />
                 <label htmlFor="username">Enter Username : </label>
-                <input id="username" type="text" placeholder="Enter your Username..." value={username} onChange={handleInputChange} name="username" />
+                <input id="username" type="text" placeholder="Enter your Username..." value={formData.username} onChange={handleInputChange} name="username" />
                 <br />  
+                <br />
+                <label htmlFor="password">Enter Password : </label>
+                <input type="password" placeholder="Enter your Password..." id="password" value={formData.password} onChange={handleInputChange} name="password" />
+                <br />
                 <br />
                 <button>Submit</button>
             </form>
