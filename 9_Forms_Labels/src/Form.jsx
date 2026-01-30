958e1e0eb84beccd4f2e1bff53b29718e3ce5248
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-30 10:28:21 +0530

    handling multiple inputs in the form using object

--- 9_Forms_Labels/src/Form.jsx
+++ 9_Forms_Labels/src/Form.jsx
@@ -1,21 +1,48 @@
 import { useState } from "react";
 
 export default function Form() {
-    let [fullName, setFullName] = useState("");
+    // let [fullName, setFullName] = useState("");
+    // let [username, setUsername] = useState("");
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: ""
+    })
+
     /*
     Mechanis of flow: i> input change, ii> onchange tiggers, iii> handleNameChange gets invoked,
     <iv>setFullName executes, v> fullName(state) values changes, vi> react re-renders due to state change
     */
-    let handleNameChange = (event) => {
-        // console.log(event.target.value);
-        setFullName(event.target.value)
+    // let handleNameChange = (event) => {
+    //     // console.log(event.target.value);
+    //     setFullName(event.target.value)
+    // }
+
+    // let handleUsername = (event) => {
+    //     setUsername(event.target.value);
+    // }
+
+    let handleInputChange = (event) => {
+        let eventName = event.target.name;
+        let eventNewValue = event.target.value;
+        setFormData((currData) => {
+            currData[eventName] = eventNewValue;
+            return {...currData};
+        })
     }
+
+
     return(
         <>
             <form>
                 {/* for is an loop reserved keyword in Js therefore "htmlFor" is used to denote so */}
-                <label htmlFor="username">Name : </label>
-                <input id="username" type="text" placeholder="Enter your name..." value={fullName} onChange={handleNameChange} />
+                <label htmlFor="fullName">Enter Name : </label>
+                <input id="fullName" type="text" placeholder="Enter your name..." value={fullName} onChange={handleInputChange} name="fullName" />
+                <br />
+                <br />
+                <label htmlFor="username">Enter Username : </label>
+                <input id="username" type="text" placeholder="Enter your Username..." value={username} onChange={handleInputChange} name="username" />
+                <br />  
+                <br />
                 <button>Submit</button>
             </form>
         </>
