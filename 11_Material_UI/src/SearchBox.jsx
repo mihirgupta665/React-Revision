6e29ca3283185a99286408288e959b79a70c97e2
Mihir Gupta <mihirgupta665@gmail.com>
2026-01-31 14:04:33 +0530

    fetching weather info from api

--- 11_Material_UI/src/SearchBox.jsx
+++ 11_Material_UI/src/SearchBox.jsx
@@ -7,15 +7,36 @@ import { useState } from "react";
 export default function SearchBox() {
 
     let [city, setCity] = useState("");
-
+    
     let handleChange = (event) => {
         setCity(event.target.value);
     }
+
+    // ?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "4fe2f28c4b2651c26045422aec92929d";
+
+    let getWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric `);    
+        let jsonResponse = await response.json();
+        // console.log(jsonResponse);
+        let result = {
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelslike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description
+        }
+        console.log(result); 
+    }
+
     
     let handleSubmit = (event) => {
         event.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo()[0];
     }
 
     return(
---
+++ test.js
@@ -0,0 +1,8 @@
+function Test(){
+    console.log(a)
+    var a =30;
+    let b = 30;
+    function point(){
+        console.log(a+b);
+    } print();
+} Test();
